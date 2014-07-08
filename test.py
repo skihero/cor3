96e708361a8b59a8309a8029868373d10750d37b
skihero <skihero@gmail.com>
2014-07-09 03:40:27 +0530

    Removed print statements

--- test.py
+++ test.py
@@ -5,10 +5,6 @@ from pytesseract import image_to_string
 
 import sys 
 
-
-print "sys.argv[]"
-print  sys.argv
 print image_to_string(Image.open(sys.argv[1]))
 
-#print image_to_string(Image.open('test-european.jpg'), lang='fra')
 
